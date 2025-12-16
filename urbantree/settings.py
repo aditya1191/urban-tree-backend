6b94d767f19738e198b3b3751b0a6785f929b26b
aditya1191 <adi789gari@gmail.com>
2025-12-16 04:01:20 -0500

    updated token issue

--- urbantree/settings.py
+++ urbantree/settings.py
@@ -140,15 +140,6 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_HEADERS = (
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
     "content-disposition"
 )
 
