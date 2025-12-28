dd8812260585c49a0906a886e3c082b6cde9dc52
aditya1191 <adi789gari@gmail.com>
2025-12-28 16:49:07 -0500

    create super user

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # uncomment the below line inorder to create a superuser and set password as env variable (one-time)
-# python manage.py createsuperuser --username=admin --email=joyJoy_Winbourne@uml.edu --noinput
+python manage.py createsuperuser --username=admin --email=joyJoy_Winbourne@uml.edu --noinput
 
 # uncomment the below line inorder to create tables in db (one-time)
-# python manage.py migrate
+python manage.py migrate
